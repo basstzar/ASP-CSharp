493d7066993f2ecb7d95c73cd3781e5691df1c77
gsvn <gsvn@bd82b0af-b8a9-4f72-82b4-5bcb1f86eeb0>
2015-01-30 13:55:37 +0000

    99 beers

---
+++ glee_week_04_strength.rb
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+number = 99
+
+while number > 0
+
+puts "#{number} bottles of beer on the wall, #{number} bottles of beer.  You take one down and pass it around #{number-1}
+bottles of beer on the wall."
+
+  number -=1
+  end
